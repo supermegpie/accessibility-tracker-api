5e3fc42db1f332226f73748c3caa929065f94165
Meg Knoernschild <megan@seattleweb.com>
2026-05-13 12:05:27 -0500

    Fix CORS to allow Vercel URL

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://accessibility-tracker-api-production.up.railway.app',
+    'https://accessibility-tracker-sooty.vercel.app',
   ],
   credentials: true
 }));
