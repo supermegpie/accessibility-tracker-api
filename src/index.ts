3c6be04481519291670bab93f19c249bd0a0f96d
Meg Knoernschild <megan@seattleweb.com>
2026-05-13 12:06:52 -0500

    Open CORS for public access

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
  
 //Only allow requests from the frontend
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://accessibility-tracker-sooty.vercel.app',
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json()); // needed to read JSON from POST requests
  
 //Needed to confirm the server is running
